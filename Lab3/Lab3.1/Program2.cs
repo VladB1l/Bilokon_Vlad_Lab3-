557bbccd5e2fe72a4f8061f74a5f835a4b829a4a
VladB1l <108106129+VladB1l@users.noreply.github.com>
2022-06-25 00:50:31 +0300

    Add files via upload

---
+++ Lab3/Lab3.1/Program2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborat3._2
+{
+    class Program2
+    {
+        static void ShowDict(Dictionary<int, string> dictionary)
+        {
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"key: {item.Key} || value: {item.Value}");
+            }
+            Console.WriteLine("\n");
+        }
+
+        static void DictSerialize(Dictionary<int, string> dictionary)
+        {
+            /*string jsonString = JsonSerializer.Serialize(dictionary);*/
+        /*    File.WriteAllText("Serialization1.json ", jsonString);  //serialization
+            Console.WriteLine(jsonString);*/
+
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary1 = new Dictionary<int, string>()
+            {
+                {1, "key1" },
+                {3, "key2" },
+                {6, "key3" },
+                {5, "key3" },
+                {4, "key11" }
+
+            };
+            Dictionary<int, string> dictionary2 = new Dictionary<int, string>()
+            {
+                {5, "key3" },
+                {1, "key1" },
+                {3, "key4" },
+                {4, "key6" },
+                {2, "key3" }
+            };
+
+            Console.WriteLine($"Словник dictionary1: ");
+            ShowDict(dictionary1);
+            Console.WriteLine($"Словник dictionary2: ");
+            ShowDict(dictionary2);
+
+            Dictionary<int, string> dictionary3 = new Dictionary<int, string>();
+
+            foreach (var item1 in dictionary1)
+            {
+                if (dictionary2.ContainsKey(item1.Key) && dictionary2.ContainsValue(item1.Value))
+                {
+                    Console.WriteLine($"key: {item1.Key} || value: {item1.Value} є в обох списках ");
+                    dictionary3.Add(item1.Key, item1.Value);
+                }
+
+            }
+
+            Console.WriteLine($"\nСформований словник dictionary3 та його данi занесенi до файлу:");
+            ShowDict(dictionary3);
+            DictSerialize(dictionary3);
+
+        }
+
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborat3
+{
+    class Program1
+    {
+        public static void PrintValues(IEnumerable numbers)
+        {
+            foreach (Object obj in numbers)
+                Console.Write(" " + obj);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть кiлькiсть людей у крузi: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            var numbers = new ArrayList();
+            for (int i = 1; i <= n; i++)
+            {
+                numbers.Add(i);
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine("Наш круг людей:");
+            PrintValues(numbers);
+            Console.WriteLine("\nВидаляється кожний другий...");
+
+            while (numbers.Count != 1)
+            {
+                int i = 1;
+                n = numbers.Count;
+                while (i < numbers.Count)
+                {
+                    numbers.RemoveAt(i);
+                    PrintValues(numbers);
+                    i++;
+
+                }
+                if (n % 2 == 1)
+                {
+                    numbers.RemoveAt(0);
+                    PrintValues(numbers);
+                }
+            }
+
+
+            /* stopWatch.Stop();
+             TimeSpan ts = stopWatch.Elapsed;
+             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+           ts.Hours, ts.Minutes, ts.Seconds,
+           ts.Milliseconds / 10);
+             Console.WriteLine("RunTime " + elapsedTime);*/
+
+
+            /*
+                        Console.WriteLine("Введiть кiлькiсть людей у крузi: ");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        LinkedList<string> numbers = new LinkedList<string>();
+                        for (int i = 1; i <= n; i++)
+                        {
+                            numbers.AddLast($"{i}");
+                        }
+                        Stopwatch stopWatch = new Stopwatch();
+                        stopWatch.Start();
+                        Console.WriteLine("Наш круг людей:");
+
+                        Console.WriteLine("\nВидаляється кожний другий...");
+                        for (int i = 1; i < numbers.Count; i++)
+                        {
+                            numbers.Remove(numbers.ElementAt(i));
+                            n = i;
+                        }
+                        while (numbers.Count != 1)
+                        {
+                            if (n == numbers.Count)
+                            {
+                                for (int k = 1; k < numbers.Count; k++)
+                                {
+                                    numbers.Remove(numbers.ElementAt(k));
+                                    n = k;
+                                }
+                            }
+                            if (n == numbers.Count - 1)
+                            {
+                                for (int k = 0; k < numbers.Count; k++)
+                                {
+                                    numbers.Remove(numbers.ElementAt(k));
+
+                                    n = k;
+                                }
+                            }
+                        }
+                        PrintValues(numbers);
+                        stopWatch.Stop();
+                        TimeSpan ts = stopWatch.Elapsed;
+                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                      ts.Hours, ts.Minutes, ts.Seconds,
+                      ts.Milliseconds / 10);
+                        Console.WriteLine("RunTime " + elapsedTime);*/
+
+        }
+    }
+}
